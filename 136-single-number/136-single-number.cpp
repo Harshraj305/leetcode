ef3afc9c9e9fb4ebf71427f9db5631ce00230abf
Harsh Raj <87377535+Harshraj305@users.noreply.github.com>
2022-02-09 09:43:33 +0530

    Time: 17 ms (77.98%), Space: 16.8 MB (85.51%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,19 +2,11 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());
-        int ans = nums[nums.size()-1];
+        int xorSum = 0;
         
-        if(nums.size() == 1) {
-            return ans;
+        for(int i=0; i<nums.size(); i++){
+            xorSum = xorSum ^ nums[i];
         }
-        for(int i=0; i<nums.size()-1; i=i+2) {
-            if(nums[i] != nums[i+1]){
-                ans = nums[i];
-                break;
-            }
-        }
-        return ans;
-        
+        return xorSum;
     }
 };
