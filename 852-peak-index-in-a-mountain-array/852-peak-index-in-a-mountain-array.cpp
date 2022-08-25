d9b006f14601e44b523ecea0d1ad79ef55bfea29
Harsh Raj <87377535+harshraj305@users.noreply.github.com>
2022-08-25 12:59:14 +0530

    Time: 192 ms (29.34%), Space: 59.6 MB (67.10%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -5,14 +5,31 @@ public:
         int i = 0, j = arr.size()-1;
         int mid;
         
-        while (i <= j) {
-            mid = (i+j)/2;
+//         while (i < j) {
+//             mid = (i+j)/2;
             
-            if (arr[mid] < arr[mid + 1]) {
+//             if (arr[mid] < arr[mid + 1]) {
+//                 i = mid +1;
+//             }
+//             else {
+//                 j = mid;
+//             }
+//         }
+        
+//         return i;
+        
+        while (i <= j) {
+            mid = (i+j) / 2;
+            
+            if (arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) {
+                return mid;
+            }
+            
+            else if (arr[mid] < arr[mid+1]) {
                 i = mid +1;
             }
             else {
-                j = mid -1;
+                j = mid-1;
             }
         }
         
